b7816921c115a6605646fa369a0314e9a00fbdd7
CristianSpatari <spataricristinel@gmail.com>
2024-07-05 14:16:30 +0300

    add(preview): new design

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-(function($) {
-    $(document).ready(function() {
-
-        $('.devices').on('click', 'a', function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.devices').addEventListener('click', function (event) {
+        if (event.target.closest('a')) {
             event.preventDefault();
-            var device_selected = $(this).attr('href').replace('#', '');
-            $('.device').removeClass().addClass('device device-' + device_selected);
-        });
+            var device_selected = event.target.closest('a').getAttribute('href').replace('#', '');
+            document.querySelectorAll('.device').forEach(function (device) {
+                device.className = 'device device-' + device_selected;
+            });
+        }
     });
-})(jQuery);
+
+    const anchors = [...document.querySelectorAll("a")];
+
+    anchors.forEach(anchor => {
+        anchor.addEventListener("click",
+            () => {
+                // remove active class from other devices
+                anchors.filter(el => el !== anchor).map(el => el.classList.remove("current"))
+
+                anchor.classList.add("current")
+            }
+        )
+    })
+});
